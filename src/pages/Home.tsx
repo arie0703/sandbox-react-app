8a8939412e2e41720f515a831fdff603b019c63f
K.A. <kaiseiarimura0703@gmail.com>
2025-03-29 10:16:11 +0900

    Reactのsandbox開発環境としての整備: Router導入

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <div className="card">
+          <p>This is Home Component.</p>
+        </div>
+      </div>
+    </>
+  );
+};
